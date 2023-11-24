5072488f7214c0aaf1920feb4772b1b82e6e35da
PARAG JAIN <87814011+paragjain15@users.noreply.github.com>
2023-11-24 17:33:08 +0530

    Added support for calling Dynamic App_ID (#3)
    * Added support for calling Dynamic App_ID
    
    * Added support for calling Dynamic App_ID
    
    * Added support for calling Dynamic App_ID
    
    * Added support for calling Dynamic App_ID
    
    * Added support for calling Dynamic App_ID
    
    * Added support for calling Dynamic App_ID
    
    * Added support for calling Dynamic App_ID
    
    * Added support for calling Dynamic App_ID
    
    * Added support for calling Dynamic App_ID
    
    * Added support for calling Dynamic App_ID
    
    * Added support for calling Dynamic App_ID

--- src/main/java/MyRunner/TestRunner.java
+++ src/main/java/MyRunner/TestRunner.java
@@ -1,10 +1,10 @@
 package MyRunner;
+
 import java.net.URL;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.TestNGCucumberRunner;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
@@ -13,63 +13,53 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 
-import org.openqa.selenium.JavascriptExecutor;
-import java.net.MalformedURLException;
-
-
-@CucumberOptions(
-        features = "src/main/java/Features/todo.feature",
-        glue = {"stepDefinitions"},
-        plugin = "json:target/cucumber-reports/CucumberTestReport.json")
+@CucumberOptions(features = "src/main/java/Features/todo.feature", glue = {
+        "stepDefinitions"}, plugin = "json:target/cucumber-reports/CucumberTestReport.json")
 
 public class TestRunner extends AbstractTestNGCucumberTests {
-	
+
     private TestNGCucumberRunner testNGCucumberRunner;
-  
+
     public static RemoteWebDriver connection;
-    
+
     @BeforeClass(alwaysRun = true)
     public void setUpCucumber() {
-    	 testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
     }
-    
+
     @BeforeMethod(alwaysRun = true)
-    @Parameters({ "deviceName", "platformVersion", "platformName" })
+    @Parameters({"deviceName", "platformVersion", "platformName"})
     public void setUpClass(String deviceName, String platformVersion, String platformName) throws Exception {
 
-    		String username = System.getenv("LT_USERNAME") == null ? "YOUR_LT_USERNAME" : System.getenv("LT_USERNAME");      //Enter your LambdaTest username at the place of YOUR_LT_USERNAME 
-    		String accesskey = System.getenv("LT_ACCESS_KEY") == null ? "YOUR_LT_ACCESSKEY" : System.getenv("LT_ACCESS_KEY");     //Enter your LambdaTest accessKey at the place of YOUR_LT_ACCESSKEY
+        String username = System.getenv("LT_USERNAME") == null ? "YOUR_LT_USERNAME" : System.getenv("LT_USERNAME");      //Enter your LambdaTest username at the place of YOUR_LT_USERNAME
+        String accesskey = System.getenv("LT_ACCESS_KEY") == null ? "YOUR_LT_ACCESSKEY" : System.getenv("LT_ACCESS_KEY");     //Enter your LambdaTest accessKey at the place of YOUR_LT_ACCESSKEY
+        String app_id = System.getenv("LT_APP_ID") == null ? "lt://proverbial-android" : System.getenv("LT_APP_ID");      //Enter your LambdaTest App ID at the place of lt://proverbial-android
+        String grid_url = System.getenv("LT_GRID_URL") == null ? "mobile-hub.lambdatest.com" : System.getenv("LT_GRID_URL");
 
-    		DesiredCapabilities capability = new DesiredCapabilities();    		
+        DesiredCapabilities capability = new DesiredCapabilities();
 
+        capability.setCapability("platformName", platformName);
+        capability.setCapability("deviceName", deviceName);
+        capability.setCapability("platformVersion", platformVersion);
+        capability.setCapability("build", "LT-appium-java-cucumber");
+        capability.setCapability("name", "Android Test");
+        capability.setCapability("isRealMobile", true);
+        capability.setCapability("app", app_id);     //Enter the app url here
+        capability.setCapability("devicelog", true);
+        capability.setCapability("autoGrantPermissions", true);
+        capability.setCapability("network", false);
+        capability.setCapability("video", true);
+        capability.setCapability("visual", true);
 
-            capability.setCapability("platformName", platformName);
-	        capability.setCapability("deviceName", deviceName);
-	        capability.setCapability("platformVersion",platformVersion);
-    		    		
-    		capability.setCapability("build", "Native App automate Demo");
-       		capability.setCapability("test", "Test Parallel");    		
-	        capability.setCapability("isRealMobile", true);
-
-            capability.setCapability("app","lt://proverbial-android");     //Enter the app url here
-            capability.setCapability("network", false);
-    		capability.setCapability("video", true);
-    		capability.setCapability("console", true);
-    		capability.setCapability("visual", true);
-
-    		String gridURL = "https://" + username + ":" + accesskey + "@mobile-hub.lambdatest.com/wd/hub";
-    		System.out.println(gridURL);
-    		connection = new RemoteWebDriver(new URL(gridURL), capability);
-    		System.out.println(capability);
-    		System.out.println(connection.getSessionId());
-}
-
+        String gridURL = "https://" + username + ":" + accesskey + "@" + grid_url + "/wd/hub";
+        connection = new RemoteWebDriver(new URL(gridURL), capability);
+    }
 
     @DataProvider
     public Object[][] features() {
         return testNGCucumberRunner.provideScenarios();
     }
- 
+
     @AfterClass(alwaysRun = true)
     public void tearDownClass() {
         testNGCucumberRunner.finish();
--- src/main/java/stepDefinitions/ToDoStepDefinition.java
+++ src/main/java/stepDefinitions/ToDoStepDefinition.java
@@ -34,42 +34,42 @@ import MyRunner.TestRunner;
 
 public class ToDoStepDefinition extends TestRunner {
 
-	public RemoteWebDriver driver = this.connection;
-	WebDriverWait wait = new WebDriverWait(driver, 30);
+    public RemoteWebDriver driver = this.connection;
+    WebDriverWait wait = new WebDriverWait(driver, 30);
 
-	
-	@Given("^user is on the App home page$")
-	public void user_already_on_home_page() {
-	System.out.println("On Home page");
-	}
 
-	@When("^click on color element$")
-	public void click_color() {
-		wait.until(ExpectedConditions.elementToBeClickable(By.id("color"))).click();
-			
-	}
+    @Given("^user is on the App home page$")
+    public void user_already_on_home_page() {
+        System.out.println("On Home page");
+    }
 
-	@Then("^click on geolocation element and navigate back$")
-	public void click_geolocation()throws Exception {
-		wait.until(ExpectedConditions.elementToBeClickable(By.id("com.lambdatest.proverbial:id/geoLocation"))).click();
-		Thread.sleep(5000);
-		driver.navigate().back();
-		
-	}
+    @When("^click on color element$")
+    public void click_color() {
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("color"))).click();
 
-	@Then("^click on text element$")
-	public void click_text() {
-		wait.until(ExpectedConditions.elementToBeClickable(By.id("Text"))).click();		
-		//wait.until(ExpectedConditions.elementToBeClickable(By.id("Browser"))).click();
-	}
+    }
 
-	@Then("^click on notification element$")
-	public void click_notification() {
-		wait.until(ExpectedConditions.elementToBeClickable(By.id("notification"))).click();
-	}
+    @Then("^click on geolocation element and navigate back$")
+    public void click_geolocation()throws Exception {
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("com.lambdatest.proverbial:id/geoLocation"))).click();
+        Thread.sleep(5000);
+        driver.navigate().back();
 
-	@Then("^click on toast element$")
-	public void click_toast() {
-		wait.until(ExpectedConditions.elementToBeClickable(By.id("toast"))).click();
-	}
+    }
+
+    @Then("^click on text element$")
+    public void click_text() {
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("Text"))).click();
+        //wait.until(ExpectedConditions.elementToBeClickable(By.id("Browser"))).click();
+    }
+
+    @Then("^click on notification element$")
+    public void click_notification() {
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("notification"))).click();
+    }
+
+    @Then("^click on toast element$")
+    public void click_toast() {
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("toast"))).click();
+    }
 }
